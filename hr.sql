a0045c1083f73420c1f158cdabc07bdc5ad038e9
KimSeokHyun <sokon112@gmail.com>
2021-04-14 18:08:47 +0900

    fail...

---
+++ hr.sql
@@ -0,0 +1,9 @@
+select * from employees;
+
+select first_name,last_name,job_id from employees;
+
+select last_name,job_id from employees where employee_id =176;
+
+select last_name,salary from employees where salary >=12000;
+
+select last_name,salary from employees where salary >12000 or salary <5000;
